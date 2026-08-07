66b7939ca4f3e9b54bcf0b2f128f612b7f183004
sanjayk36725 <sanjaykumargovindharaj@gmail.com>
2026-08-07 10:29:44 +0530

    Step 11: interaction_step

--- model.py
+++ model.py
@@ -69,8 +69,27 @@ def q_learning_update(q_table, state, action, reward,
 
     return float(q_table[state, action])
 
-# Step 11 - interaction_step (not yet solved)
-# TODO: implement
+# Step 11 - interaction_step
+def interaction_step(env, q_table, state, epsilon, alpha, gamma, rng):
+    action = epsilon_greedy_action(
+        q_table, state, epsilon, env.action_space, rng
+    )
+
+    next_state, reward, terminated, truncated, _ = env.step(action)
+    done = terminated or truncated
+
+    q_learning_update(
+        q_table,
+        state,
+        action,
+        reward,
+        next_state,
+        done,
+        alpha,
+        gamma
+    )
+
+    return int(next_state), float(reward), bool(done)
 
 # Step 12 - run_training_episode (not yet solved)
 # TODO: implement
